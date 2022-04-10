ae6fc37abe06eb35c6bdef8a19ecabe97509cb5d
senluoye <57495136+senluoye@users.noreply.github.com>
2022-04-10 15:10:29 +0800

    Initial commit

---
+++ src/main/java/com/example/mynovelserver/MyNovelServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.mynovelserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyNovelServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyNovelServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mynovelserver/MyNovelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mynovelserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyNovelServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
